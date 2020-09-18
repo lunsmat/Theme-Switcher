fe2cd6273dc143a7ba9fe0085858fd43db08f3ec
Luan Farias <luan.farias_bvs@outlook.com>
2020-09-18 11:07:33 -0300

    Updates

---
+++ precache-manifest.94bd24f8b130dc045725f19f0410630f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6711cdca0c8c478c796aa1d6d982ae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7179395948752aff6e0e",
+    "url": "/static/js/2.3c9b0b15.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.3c9b0b15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d44ec28f3ad0c195b85",
+    "url": "/static/js/main.5702d138.chunk.js"
+  },
+  {
+    "revision": "d597e7259f4092392642",
+    "url": "/static/js/runtime-main.507abb98.js"
+  }
+]);
